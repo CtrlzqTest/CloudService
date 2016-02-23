c76a0872c4ff88ac824c198ffe0f800047a0e847
Ctrlzq <15027164687@163.com>
2016-02-23 11:35:25 +0800

    登录界面

---
+++ CloudService/CloudService/Controllers/LoginViewController.m
@@ -0,0 +1,63 @@
+//
+//  LoginViewController.m
+//  CloudService
+//
+//  Created by zhangqiang on 16/2/23.
+//  Copyright © 2016年 zhangqiang. All rights reserved.
+//
+
+#import "LoginViewController.h"
+
+@interface LoginViewController ()
+
+@property (weak, nonatomic) IBOutlet UIView *inputView;
+@property (weak, nonatomic) IBOutlet UIButton *loginBtn;
+
+
+@end
+
+@implementation LoginViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self setupView];
+}
+
+-(void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [self.navigationController setNavigationBarHidden:YES animated:YES];
+}
+
+- (void)setupView {
+    self.inputView.layer.cornerRadius = 5;
+    self.inputView.clipsToBounds = YES;
+    
+    self.loginBtn.layer.cornerRadius = 3;
+    self.loginBtn.clipsToBounds = YES;
+    self.loginBtn.layer.borderWidth = 1;
+    self.loginBtn.layer.borderColor = [UIColor colorWithRed:0.356 green:0.744 blue:0.036 alpha:1.000].CGColor;
+    
+}
+
+// 登录
+- (IBAction)loginAction:(id)sender {
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
