fcd48284a72236eb3a2668827e150a266e1fcb88
Ctrlzq <15027164687@163.com>
2016-02-26 11:00:08 +0800

    首页

---
+++ CloudService/CloudService/Views/HomeCollectionCell.m
@@ -0,0 +1,19 @@
+//
+//  HomeCollectionCell.m
+//  CloudService
+//
+//  Created by zhangqiang on 16/2/26.
+//  Copyright © 2016年 zhangqiang. All rights reserved.
+//
+
+#import "HomeCollectionCell.h"
+
+@implementation HomeCollectionCell
+
+- (void)awakeFromNib {
+    
+    self.backgroundColor = [UIColor whiteColor];
+    
+}
+
+@end
