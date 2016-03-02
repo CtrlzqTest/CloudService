6c5b4c31f95598c097c967e5fd44141961f9e353
Ctrlzq <15027164687@163.com>
2016-03-02 17:51:10 +0800

    适配

--- CloudService/CloudService/Views/HomeCollectionCell.m
+++ CloudService/CloudService/Views/HomeCollectionCell.m
@@ -8,11 +8,19 @@
 
 #import "HomeCollectionCell.h"
 
+@interface HomeCollectionCell()
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *topCanstraint;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *imgWidth;
+
+@end
+
 @implementation HomeCollectionCell
 
 - (void)awakeFromNib {
     
     self.backgroundColor = [UIColor whiteColor];
+    self.topCanstraint.constant = 20.0 / 667 * KHeight;
+    self.imgWidth.constant = 40.0 / 417 * KWidth;
     
 }
 
